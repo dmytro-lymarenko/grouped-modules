f965560461c627e58ea5db15ab4cb2c3565d49b6
Dmytro <dmytro.lymarenko@gmail.com>
2016-01-19 23:01:14 +0200

    make refactoring

--- index.js
+++ index.js
@@ -30,68 +30,54 @@ function gmInDeep(parentGroupName, children) {
 	}
 }
 
-function gm(groupName) {
-	let typeofGroupName = (typeof groupName);
-	if(typeofGroupName === 'undefined') {
-		throw new Error('Not specified group name as string or groups as object or array');
-	}
+function gmString(groupName) {
+	return {
+		getPath(modulePath) {
+			if(groups[groupName] === undefined) {
+				throw new Error(`There is no group with '${groupName}' name. Please use gm('${groupName}').assignTo('path/to/group')`);
+			}
+			if(modulePath === undefined) {
+				return groups[groupName];
+			}
+			return path.resolve(groups[groupName], modulePath);
+		},
 
-	if(!(typeofGroupName === 'string'
-		|| (typeofGroupName === 'object' && !(groupName instanceof RegExp))
-		|| Array.isArray(groupName))) {
-		throw new TypeError('Group name must be a string, object or array');
-	}
+		require(modulePath) {
+			return require(this.getPath(modulePath));
+		},
 
-	if(typeofGroupName === 'string') {
-		return {
-			getPath(modulePath) {
-				if(groups[groupName] === undefined) {
-					throw new Error(`There is no group with '${groupName}' name. Please use gm('${groupName}').assignTo('path/to/group')`);
-				}
-				if(modulePath === undefined) {
-					return groups[groupName];
-				}
-				return path.resolve(groups[groupName], modulePath);
-			},
+		assignTo(groupPath) {
+			if(typeof groupPath !== 'string') {
+				throw new TypeError('The group path must be a string');
+			}
+			groups[groupName] = path.resolve(getDirName(), groupPath);
+		},
 
-			require(modulePath) {
-				return require(this.getPath(modulePath));
-			},
-
-			assignTo(groupPath) {
-				if(typeof groupPath !== 'string') {
-					throw new TypeError('The group path must be a string');
-				}
-				groups[groupName] = path.resolve(getDirName(), groupPath);
-			},
-
-			in(parentGroupName) {
-				if(typeof parentGroupName !== 'string') {
-					throw new TypeError('The parent group name must be a string');
-				}
-				if(groups[parentGroupName] === undefined) {
-					throw new Error(`There is no parent group with '${parentGroupName}' name. Please use gm('${parentGroupName}').assignTo('path/to/group')`);
-				}
-				if(groupName === parentGroupName) {
-					throw new Error('The parent group name must not be equal to group name');
-				}
-				return {
-					assignTo(groupPath) {
-						if(typeof groupPath !== 'string') {
-							throw new TypeError('The group path must be a string');
-						}
-						groups[groupName] = path.resolve(groups[parentGroupName], groupPath);
+		in(parentGroupName) {
+			if(typeof parentGroupName !== 'string') {
+				throw new TypeError('The parent group name must be a string');
+			}
+			if(groups[parentGroupName] === undefined) {
+				throw new Error(`There is no parent group with '${parentGroupName}' name. Please use gm('${parentGroupName}').assignTo('path/to/group')`);
+			}
+			if(groupName === parentGroupName) {
+				throw new Error('The parent group name must not be equal to group name');
+			}
+			return {
+				assignTo(groupPath) {
+					if(typeof groupPath !== 'string') {
+						throw new TypeError('The group path must be a string');
 					}
+					groups[groupName] = path.resolve(groups[parentGroupName], groupPath);
 				}
 			}
-		};
-	}
-	if(!Array.isArray(groupName)) {
-		gm([groupName]);
-	}
-	let arr = groupName;
-	for(let i = 0; i < arr.length; i++) {
-		let group = arr[i];
+		}
+	};
+}
+
+function gmArray(groups) {
+	for(let i = 0; i < groups.length; i++) {
+		let group = groups[i];
 		if(typeof group.name !== 'string') {
 			throw new TypeError('The \'name\' property must be a string');
 		}
@@ -109,6 +95,28 @@ function gm(groupName) {
 			}
 		}
 	}
+}
+
+function gm(value) {
+	let typeofValue = (typeof value);
+
+	if(typeofValue === 'undefined') {
+		throw new Error('Not specified group name as string or groups as object or array');
+	}
+
+	if(typeofValue === 'string') {
+		return gmString(value);
+	}
+
+	if(Array.isArray(value)) {
+		return gmArray(value);
+	}
+
+	if(typeofValue === 'object' && !(value instanceof RegExp)) {
+		return gmArray([value]);
+	}
+
+	throw new TypeError('First parameter must be a string, object or array');
 }
 
 module.exports = gm;
